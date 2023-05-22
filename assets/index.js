23c77d6142d1f706e47b45a774a37e598c540c60
erlison61 <souza.goncalves@academico.ifpb.edu.br>
2023-05-22 11:51:24 -0300

    refatoração do js

--- assets/index.js
+++ assets/index.js
@@ -2,32 +2,35 @@ const getElement = (...queries) => document.querySelector(...queries);
 const ctx = getElement('#myChart').getContext('2d');
 
 // Função para obter a cotação atual do dólar em relação ao real
-async function obterCotacaoDolarReal() {
-  try {
-    const response = await fetch('https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados/ultimos/1?formato=json');
-    const data = await response.json();
-    const cotacaoDolarReal = data[0].valor;
-    return cotacaoDolarReal;
-  } catch (error) {
-    console.log('Erro ao obter a cotação do dólar em relação ao real:', error);
-  }
+const obterCotacaoDolarReal = async() => {
+  return fetch('https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados/ultimos/1?formato=json')
+    .then(response => response.json())
+    .then(data => {
+      const cotacaoDolarReal = data[0].valor;
+      return cotacaoDolarReal;
+    })
+    .catch(error => {
+      console.log('Erro ao obter a cotação do dólar em relação ao real:', error);
+    });
 }
 
+
 // Função para obter o histórico de cotações do dólar nos últimos 7 dias
-async function obterHistoricoCotacoesDolar() {
-  try {
+const obterHistoricoCotacoesDolar= async () =>{
     const hoje = new Date();
     const dataSeteDiasAtras = new Date();
     dataSeteDiasAtras.setDate(dataSeteDiasAtras.getDate() - 7);
 
     const url = `https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados?formato=json&dataInicial=${dataSeteDiasAtras.toISOString()}&dataFinal=${hoje.toISOString()}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    const cotacoes = data.map(item => item.valor);
-    return cotacoes.slice(-7); // Retorna apenas os últimos 7 valores
-  } catch (error) {
-    console.log('Erro ao obter o histórico de cotações do dólar:', error);
-  }
+    return await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const cotacoes = data.map(item => item.valor);
+        return cotacoes.slice(-7); // Retorna apenas os últimos 7 valores
+      })
+      .catch (error =>{
+        console.log('Erro ao obter o histórico de cotações do dólar:', error);
+      })
 }
 
 // Função para atualizar o gráfico com o histórico de cotações do dólar em relação ao real
@@ -65,7 +68,7 @@ async function atualizarGrafico() {
         scales:{
             y:{
                 min:0,
-                max:5,
+                max:10,
             },
         },
     },
